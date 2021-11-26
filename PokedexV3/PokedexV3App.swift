befcc6a10323b5e4168039eabf5fa42849b5105d
anthony polka <anthonypolka@gmail.com>
2021-11-26 12:42:04 -0600

    Initial Commit

---
+++ PokedexV3/PokedexV3App.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexV3App.swift
+//  PokedexV3
+//
+//  Created by Anthony Polka on 11/26/21.
+//
+
+import SwiftUI
+
+@main
+struct PokedexV3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
